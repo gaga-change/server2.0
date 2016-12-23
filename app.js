b50a94590bdd11cc7af83f92319009aa29a73090
严俊东 <1172482914@qq.com>
2016-12-23 23:45:00 +0800

    基础设施
    搭建基础设施

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var compression = require('compression');
+
+var app = express();
+//设置端口号
+process.env.PORT = 8080;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'public'));
+app.engine('.html',ejs.renderFile); // 配置模板
+app.set('view engine', 'html');  //设置模板
+
+// app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
